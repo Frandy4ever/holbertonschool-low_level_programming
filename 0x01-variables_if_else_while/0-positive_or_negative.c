5602cf89c361c9db23d92174d0416c4b13268713
Frandy4ever <frandy4ever@gmail.com>
2022-09-19 09:23:25 -0700

    first file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+#include <stddlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* Betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+return (0)
+}
+
