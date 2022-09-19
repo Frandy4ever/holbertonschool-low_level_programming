ef5493a90ca38b1bbdc919c703938783ff44a79a
Frandy4ever <frandy4ever@gmail.com>
2022-09-18 17:51:56 -0700

    C program that prints exactly a specific requirement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\n"with proper gramar, but the outcome is a piece of art, ");
+
+	return (0);
+}
