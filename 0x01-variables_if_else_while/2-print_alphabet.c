331d6c8836ee114cab5efcfce2a3a4063efa8436
Frandy4ever <frandy4ever@gmail.com>
2022-09-19 12:07:56 -0700

    2nd update to file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+char frandy;
+
+for (frandy = 'a'; frandy <= 'z'; frandy++)
+
+putchar(frandy);
+
+putchar('\n');
+
+return (0);
+}
