447892f5ee102f8f7cba226cede9167cf6209644
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 15:00:00 -0700

    3rd update to file 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,7 +7,7 @@
 
 /**
  * main - Prints all single digit numbers of base 10 starting from 0
- *
+ * only using putchar
  * Return: Always 0
  */
 
@@ -19,11 +19,8 @@ int frandy;
 
 for (frandy = 0; frandy < 10; frandy++)
 
-{
+putchar((frandy % 10) + '0');
 
-putchar(frandy);
-
-}
 
 putchar('\n');
 
