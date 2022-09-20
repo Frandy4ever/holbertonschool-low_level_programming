b85a7fd2024f17bc923e9a06ad778bf5025d993c
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 13:22:48 -0700

    8th update to file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+
+for (int frandy = 0; frandy <= 10; frandy++)
+
+
+printf("%d", frandy);
+
+printf("\n");
+
+return (0);
+
+}
