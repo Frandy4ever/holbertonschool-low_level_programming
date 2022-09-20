bd089113944371f3d7bb5d76e3bdfe4f78c7c083
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 16:52:04 -0700

    5th update to file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@
 /**
  * main - Prints all possible combinations of single-digit numbers
  * separated by ',' followed by a space using only putchar
- *
  * Return : Always 0.
  */
 
 int main(void)
+
 {
 
 int frandy;
