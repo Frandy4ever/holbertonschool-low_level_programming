d6dc612b2355976f56d69728a03191b3d667e842
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 15:44:05 -0700

    2nd update to file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse using only putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+char frandy;
+
+for (frandy = 'z'; frandy >= 'a'; frandy++)
+
+putchar(frandy);
+
+
+putchar('\n');
+
+return (0);
+
+}
