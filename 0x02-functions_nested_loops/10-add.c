c64231ec37d1931078e370583d763f9d83f0593c
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 21:36:43 -0700

    1st update to file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+/*
+ * File: 10-add.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition of a and b
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
