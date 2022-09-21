acfa0330697d017f0567beead1412841cc792d3e
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 20:56:16 -0700

    2nd update to file 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+/*
+ * File: 3-islower.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int frandy)
+{
+
+if (frandy >= 'a' && frandy <= 'z')
+return (1);
+
+else
+return (0);
+
+}
