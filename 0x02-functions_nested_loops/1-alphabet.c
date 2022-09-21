d9206c68df0a809d15a5b4c1e5eae0fb19e1dd98
Frandy4ever <frandy4ever@gmail.com>
2022-09-20 20:20:17 -0700

    2nd update to file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet, in lowercase with putchar
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+char frandy;
+
+for (frandy - 'a'; frandy <= 'z'; frandy++)
+
+{
+
+_putchar(frandy);
+
+}
+
+_putchar('\n');
+
+}
