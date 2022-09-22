9fd87194788e8e699eee07fec9b21b49691aec68
Frandy4ever <frandy4ever@gmail.com>
2022-09-21 18:40:09 -0700

    1st update to file 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+/*
+ * File: 6-abs.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @frandy: integer to check
+ *
+ * Return: Always value of int
+ */
+
+int _abs(int frandy)
+
+{
+
+int frandy = 0;
+
+if (frandy < 0)
+{
+putchar(frandy * (-1));
+}
+putchar('\');
+
+return (frandy);
+}
