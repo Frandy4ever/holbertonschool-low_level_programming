d789c86c42d6aa2783d27f35f76a6227dbdfd6cd
Frandy4ever <frandy4ever@gmail.com>
2022-09-25 21:18:37 -0700

    1st update to file 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-isdigit.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: digit to check for
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+
+if (isdigit(c))
+return (1);
+
+else
+return (0);
+}
