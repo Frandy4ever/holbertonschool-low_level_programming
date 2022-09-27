51ab476a6c7ac5dd720522fb0ef850a4f6345802
Frandy4ever <frandy4ever@gmail.com>
2022-09-27 16:35:18 -0700

    4th update to file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+/*
+ * File: 6-puts2.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: character to print
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+
+int i = 0;
+
+while (*(str + i))
+{
+
+_putchar(*(str + i));
+i = i + 2;
+}
+
+_putchar('\n');
+
+}
