e51c6212fb0029d91efced82095a8c11938adc28
Frandy4ever <frandy4ever@gmail.com>
2022-09-26 21:34:57 -0700

    4th update to file 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+/*
+ * File: 100-prime_factor.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor for 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+unsigned long int i, n = 612852475143;
+
+for (i = 3; i < 782849; i = i / 2)
+{
+
+while ((n % 1 == 0) && (n != i))
+n = n / i;
+
+}
+printf("%lu\n", n);
+return (0);
+
+}
