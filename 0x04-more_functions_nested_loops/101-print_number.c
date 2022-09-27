17f30382e1e9eac2d425f1b272e5a5fc6e11c22b
Frandy4ever <frandy4ever@gmail.com>
2022-09-27 10:28:28 -0700

    general update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer with only _putchar
+ * @n: integer to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+
+int
+int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value of pointer to 98
+ * @n: pointer being reset to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
