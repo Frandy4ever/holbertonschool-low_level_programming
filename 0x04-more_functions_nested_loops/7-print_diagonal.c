896655b91317919841aaffa1c245375c5d6a118c
Frandy4ever <frandy4ever@gmail.com>
2022-09-26 20:39:28 -0700

    1st update to file 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+/*
+ * File: 7-print_diagonal.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - prints a diagonal line of length n
+ * @n: length of diagonal
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+
+int a, b;
+
+if (a <= 0)
+_putchar('\n');
+
+for (a = 0; a < n; a++)
+
+for (b = 0; b < a; b++)
+{
+_putchar(' ');
+}
+
+_putchar('\\');
+_putchar('\n');
+
+}
+
