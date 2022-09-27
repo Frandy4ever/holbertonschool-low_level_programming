5f0fc5d68a474be028b56e5489ef08d0962837ec
Frandy4ever <frandy4ever@gmail.com>
2022-09-26 18:22:15 -0700

    4th update to file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-mul.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two digits
+ * @a: 1st int to use
+ * @b: 2nd int to use
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
