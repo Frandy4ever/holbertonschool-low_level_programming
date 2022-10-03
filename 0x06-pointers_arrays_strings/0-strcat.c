771127f73b89a13babb2e9dc21775fbabb997472
Frandy4ever <frandy4ever@gmail.com>
2022-10-03 11:04:38 -0700

    general update to all files and directories

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+ #include "main.h"
+ #include <stdio.h>
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: string to be appended to
+ * @src: string that will appended
+ *
+ * Return: Pointer to string dest
+ */
+
+char *_strcat(char *dest, char *src)
