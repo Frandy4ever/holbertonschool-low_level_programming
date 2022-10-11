6fb8983314b9fd38d00ad9e54668043121f8bb7c
Frandy4ever <frandy4ever@gmail.com>
2022-10-11 14:48:13 -0700

    3rd update to file 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints string in reverse
+ * @s: Sting to print in rev
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if(*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
