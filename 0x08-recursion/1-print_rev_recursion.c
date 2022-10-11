7f1a3bc2fd6a688dd8556ea2fee612683b5cfcb1
Frandy4ever <frandy4ever@gmail.com>
2022-10-11 15:02:20 -0700

    3rd update to file 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -15,9 +15,18 @@
 void _print_rev_recursion(char *s)
 {
 
-	if(*s)
+	if (*s)
 	{
-		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		_print_rev+recursion(s - 1);
 	}
+	else
+		_putchar('\n');
+	
+
+
+
+
+
+
 }
