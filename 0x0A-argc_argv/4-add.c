5458e84d85c1cf5b10433c903bd397c1b5a06533
Frandy4ever <frandy4ever@gmail.com>
2022-10-11 21:11:21 -0700

    update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add 2
+ * @argc: argument
+ * @argv: argument
+ * Description: if no numberis passed
+ *
+ * Return: 1 or 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int total, i;
+
+	char *p;
+
+	int num;
+
+	total = 0;
+
+	if (argc > 1)
+	{
+	for (i = 1; argv[i]; i++)
+	{
+		num = strtol(argv[i], &p, 10);
+		if (!*p)
+			total += num;
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	}
+	printf("%d\n", total);
+	return (0);
+}
