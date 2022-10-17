381bc2d473c2ca5a51538611df7eed4852de61e4
Frandy4ever <frandy4ever@gmail.com>
2022-10-17 10:09:10 -0700

    general update to all files

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,19 @@
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+
+/**
+ * Desc: Header file that defines a macro named
+ *		SIZE as an abbreviation for the token 1024
+ */
+
+
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+
+int _putchar(char);
+
+
+
+#endif
