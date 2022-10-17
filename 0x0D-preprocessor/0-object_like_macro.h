2fe4c09d10d1ef72f25ff72a74ce991759a55ccb
Frandy4ever <frandy4ever@gmail.com>
2022-10-17 10:25:33 -0700

    9th update to file 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -10,8 +10,8 @@
  */
 
 
-#ifndef _0_OBJECT_LIKE_MACRO_H
-#define _0_OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 //#include "_0_OBJECT_LIKE_MACRO_H"
 //#include  "_0_OBJECT_LIKE_MACRO_H"
