d19092259a47c2f2ced16f50d347e5c9cbfd69c7
Frandy4ever <frandy4ever@gmail.com>
2022-10-17 10:57:25 -0700

    1st update to file 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-main.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the compiling file
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+}
