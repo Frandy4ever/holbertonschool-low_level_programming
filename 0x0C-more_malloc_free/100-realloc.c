abd0118beedbc63ede4493953481ba6ce3cce1ad
Frandy4ever <frandy4ever@gmail.com>
2022-10-17 13:38:23 -0700

    3rd update to file 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,32 +21,33 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		unsigned int i, max = new_size;
 		char *vieux = ptr;
 
-		if (ptr == NULL)
-			{
-				p = malloc(new_size);
-				return (p);
-			}
-			else if (new_size == 0)
-			{
-		free(ptr);
-		return (NULL);
-			}
-		
-		else if (new_size == old_size)
-		return (ptr);
+if (ptr == NULL)
+{
 
-		p = malloc(new_size);
-		
-		if (p == NULL)
-		return (NULL);
-									
-		if (new_size > old_size)
-		max = old_size;
-											
-		for (i = 0; i < max; i++)
-		p[i] = vieux[i];
-		free(ptr);
-		return (p);
+p = malloc(new_size);
+return (p);
+}
+else if (new_size == 0)
+{
+free(ptr);
+return (NULL);
 }
 
-	
+else if (new_size == old_size)
+return (ptr);
+
+p = malloc(new_size);
+
+if (p == NULL)
+return (NULL);
+
+if (new_size > old_size)
+max = old_size;
+
+for (i = 0; i < max; i++)
+p[i] = vieux[i];
+free(ptr);
+return (p);
+}
+
+
