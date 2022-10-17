33a1719a9513a943781a3285cc30f09cb434ffb2
Frandy4ever <frandy4ever@gmail.com>
2022-10-17 13:08:51 -0700

    general update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-malloc_checked.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process
+ *		terminate if error.
+ */
+
+void *malloc_checked(insigned int b)
+{
+
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+
+		exit(98);
+
+	return (p);
+
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+/*
+ * File: 3-array_range.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: first integer number
+ * @max: last int number
+ *
+ * Return: pointer to newly allocated memory or NULL if fails
+ */
+
+int *array_range(int min, int max)
+{
+
+	int i,f;
+	int *h
+
+		if(min > max)
+			return (NULL);
+
+	f = max - min + 1;
+	h = malloc(sizeof(int) * f);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < f; i++, min++)
+	{
+		a[i] = min;
+	}
+	
+	return (a);
+
+}
