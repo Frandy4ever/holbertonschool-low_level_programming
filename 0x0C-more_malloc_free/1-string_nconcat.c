6598045752f5af9e95ca27489c937f973567b9c2
Frandy4ever <frandy4ever@gmail.com>
2022-10-18 12:38:05 -0700

    19th update to file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,8 @@
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+	char *s;
+	char *no = malloc(5);
 {
 
 	unsigned int i, j, k;
@@ -56,4 +58,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (s);
 
-}
+return}
