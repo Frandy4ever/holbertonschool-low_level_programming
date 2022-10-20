bb3b46946c09213412a84b8a4b82828999d92e53
Frandy4ever <frandy4ever@gmail.com>
2022-10-20 10:29:25 -0700

    1st update to file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - structure defination
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char* name;
+	char* owner;
+	float age;
+};
+
+
+#endif
