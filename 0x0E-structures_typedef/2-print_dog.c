76d1d8aafcf21af1d9cd8a971a9d02366f862794
Frandy4ever <frandy4ever@gmail.com>
2022-10-20 12:53:40 -0700

    4th update to file 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,16 +15,16 @@ if (d == NULL)
 if (d->name == NULL)
 	printf("Name: (nil)\n");
 else
-	printf("%s\n", d->name);
+	printf("Name: %s\n", d->name);
 
 if (d->age != d->age)
 	printf("Name: (nil)\n");
 else
-	printf("%f\n", d->age);
+	printf("Name: %f\n", d->age);
 
 if (d->owner == NULL)
 	printf("Name: (nil)\n");
 else
-	printf("%s\n", d->owner);
+	printf("Name: %s\n", d->owner);
 
 }
