a999ad8a2941d0faef7804d1f4ab72a329e15be5
Frandy4ever <frandy4ever@gmail.com>
2022-10-21 05:35:13 -0700

    1st update to file 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,24 @@
+/*
+ * File: 3-main.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the result of simple operations
+ * @argc: number of arg supplied to the program
+ * @argv: an array of pointers to the arg
+ *
+ * Return: Always 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+
+
+
+
+}
