62ee28e501447604989a2d5aa77c2c698cee5320
Frandy4ever <frandy4ever@gmail.com>
2022-10-20 19:58:31 -0700

    4th update to file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,10 @@
  * @name: name of the dog
  * @owner: owner of the dog
  * @age: age of the dog
+ * dog_t - new name assigned to struct dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
@@ -17,6 +18,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
