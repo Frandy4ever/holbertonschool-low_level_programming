a9b30da077cdcc63df8aea4b606de4fce34c7a0f
Frandy4ever <frandy4ever@gmail.com>
2022-11-06 15:27:32 -0800

    write a program that prints something before that main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+/*
+ * File: 100-first.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdio.h>
+
+void __attribute__((constructor)) here(void);
+
+/**
+ * here - Prints a string before the main functuon is executed
+ */
+
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
