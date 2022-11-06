bf5a4e0d3cb171a761688417a62e3b47b19341dc
Frandy4ever <frandy4ever@gmail.com>
2022-11-06 15:37:34 -0800

    write a program that prints before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,6 +13,5 @@ void __attribute__((constructor)) here(void);
 
 void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\\n""I bore my house upon my back!\\n");
 }
