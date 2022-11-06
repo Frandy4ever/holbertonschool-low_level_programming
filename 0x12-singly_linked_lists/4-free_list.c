b6b66555013ff87a9224e3eca77b37bcd254dfe9
Frandy4ever <frandy4ever@gmail.com>
2022-11-06 15:18:05 -0800

    create program tha frees list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-free_list.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "list.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees a linked list node
+ * @head: A first node in the linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
