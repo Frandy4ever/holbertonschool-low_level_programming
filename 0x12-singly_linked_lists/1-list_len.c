5c2d7a9b4657d8545d4c7ef5487edb887d95cf91
Frandy4ever <frandy4ever@gmail.com>
2022-11-06 14:22:37 -0800

    return number of nodes in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+/*
+ * File: 1-list_len.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include "list.h"
+
+/**
+ * list_len - Finds the number of elements in a linked list
+ * @h: The linked list
+ *
+ * Return: The number of elements at point h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t linkedNodes = 0;
+
+	while (h)
+	{
+		linkedNodes++;
+		h = h->next;
+	}
+
+	return (linkedNodes);
+}
