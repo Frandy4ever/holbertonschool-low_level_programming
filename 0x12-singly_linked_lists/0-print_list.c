fe5d0a78ee5eab278cdc25ef2eb4113c6dccba83
Frandy4ever <frandy4ever@gmail.com>
2022-11-06 13:23:56 -0800

    create program that prints the nodes of list_t

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+/*
+ * File: 0-print_list.c
+ * Auth: Frandy Slueue @holbertonschool
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * size_t print_list - Prints all elements of a list_t list
+ * @h: pointer to the first node in the link list
+ *
+ * Return: Number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+
+	while (h)
+	{
+		if (h->str == NULL)
+
+			printf("[0] (nil)\n");
+
+		else
+
+			printf("[%d] %s\n", h->len, h->str);
+
+		node++;
+		h = h->next;
+
+	}
+
+	return (node);
+}
