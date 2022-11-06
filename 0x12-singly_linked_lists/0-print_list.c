520b4a31a0eecea3bbc75c83900b423fc3dfe4be
Frandy4ever <frandy4ever@gmail.com>
2022-11-06 14:09:44 -0800

    4th update to program that prints linked list nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,13 +3,12 @@
  * Auth: Frandy Slueue @holbertonschool
  */
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "list.h"
+#include <stdio.h>
+
 
 /**
- * size_t print_list - Prints all elements of a list_t list
+ * size_t print_list - Prints all elements of a list_t link list
  * @h: pointer to the first node in the link list
  *
  * Return: Number of nodes in the list
@@ -19,6 +18,19 @@ size_t print_list(const list_t *h)
 {
 	size_t listNode = 0;
 
+/*
+ * while (h != NULL)
+ * {
+ *
+ *	if (h->str == NULL)
+ *	{
+ *	printf("[[0] (nil)\n");
+ *	}
+ *	printf("[%d] %s\n", h->len, h->str);
+ *	listNode++;
+ *	h = h->next;
+ * }
+ */
 
 	while (h)
 	{
